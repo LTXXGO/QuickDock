0c01cce1bfb380d034feecc5d7ad056c6768a358
LTXX <ltxxgo@icloud.com>
2019-04-23 19:20:25 +0800

    Initial commit.

---
+++ QuickDock/ViewController/添加/AddNewImageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddNewImageViewController.swift
+//  QuickDock
+//
+//  Created by LTXX on 2019/4/23.
+//  Copyright © 2019 LTXX. All rights reserved.
+//
+
+import UIKit
+
+class AddNewImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
