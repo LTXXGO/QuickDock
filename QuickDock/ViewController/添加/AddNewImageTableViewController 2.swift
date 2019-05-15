9bf897cfc8c439175059e0173bfd9a429096d80d
LTXX <ltxxgo@icloud.com>
2019-05-15 19:56:06 +0800

    增加了从相机/相册获取图片的方法

---
+++ QuickDock/ViewController/添加/AddNewImageTableViewController 2.swift
@@ -0,0 +1,30 @@
+//
+//  AddNewImageTableViewController.swift
+//  QuickDock
+//
+//  Created by LTXX on 2019/4/27.
+//  Copyright © 2019 LTXX. All rights reserved.
+//
+
+import UIKit
+
+class AddNewImageTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 0 {
+            return 300
+        } else if indexPath.section == 4 {
+            return 140
+        } else {
+            return 44
+        }
+    }
+}
